3c82f78aa3da7861c19af6cf2928d3c83a55dc0f
Christopher Kodama <ckodama@redhat.com>
2025-05-30 15:56:57 -0400

    add authentication for sealights-helper

--- sealights/sealights-helper.sh
+++ sealights/sealights-helper.sh
@@ -1,12 +1,13 @@
-apk add cosign jq
-
 function get_sealights_image {
   REAL_QUAY_URI=$1
   SEALIGHTS_BUILD_STEP=$2
 
+  ### get the correct credential
+  mkdir -p /tmp/auth && select-oci-auth "$REAL_QUAY_URI" >/tmp/auth/config.json
   ### Get the attestation info from the actual build URI
-  SEALIGHTS_BUILD_RESULTS=$(cosign download attestation $REAL_QUAY_URI \
-    | jq '.payload|@base64d|fromjson' \
+  DOCKER_CONFIG=/tmp/auth cosign download attestation $REAL_QUAY_URI > attestation.json
+
+  SEALIGHTS_BUILD_RESULTS=$(jq '.payload|@base64d|fromjson' attestation.json \
     | jq -r --arg N "$SEALIGHTS_BUILD_STEP" '.predicate.buildConfig.tasks[] | select(.name == $N)| .results')
 
   ### Find the sealights build URI from the attestation
@@ -18,4 +19,6 @@ function get_sealights_image {
   SEALIGHTS_IMAGE_REPO=$(echo $SEALIGHTS_BUILD_RESULTS | jq -r '.[] | select(.name == "IMAGE_URL") | .value' | sed 's/:.*//' | head -n 1)
 
   echo "${SEALIGHTS_IMAGE_REPO}@${SEALIGHTS_IMAGE_DIGEST}"
+  rm attestation.json
+  rm /tmp/auth/config.json
 }
