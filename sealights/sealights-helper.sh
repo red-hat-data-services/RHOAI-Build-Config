dca339f99089bbc1d572899e8779f39c50c29e4e
Christopher Kodama <ckodama@redhat.com>
2025-05-30 13:51:20 -0400

    add scripts to inject sealights into bundle

---
+++ sealights/sealights-helper.sh
@@ -0,0 +1,19 @@
+function get_sealights_image {
+  REAL_QUAY_URI=$1
+  SEALIGHTS_BUILD_STEP=$2
+
+  ### Get the attestation info from the actual build URI
+  SEALIGHTS_BUILD_RESULTS=$(cosign download attestation $REAL_QUAY_URI \
+    | jq '.payload|@base64d|fromjson' \
+    | jq -r --arg N "$SEALIGHTS_BUILD_STEP" '.predicate.buildConfig.tasks[] | select(.name == $N)| .results')
+
+  ### Find the sealights build URI from the attestation
+
+  # this has the form 'sha256:<digest>'
+  SEALIGHTS_IMAGE_DIGEST=$(echo $SEALIGHTS_BUILD_RESULTS | jq -r '.[] | select(.name == "IMAGE_DIGEST") | .value' | head -n 1)
+
+  # using sed to remove the tag, leaving just the repo url
+  SEALIGHTS_IMAGE_REPO=$(echo $SEALIGHTS_BUILD_RESULTS | jq -r '.[] | select(.name == "IMAGE_URL") | .value' | sed 's/:.*//' | head -n 1)
+
+  echo "${SEALIGHTS_IMAGE_REPO}@${SEALIGHTS_IMAGE_DIGEST}"
+}
